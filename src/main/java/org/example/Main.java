b0e78ea2b8fee7ef1b919c73fdb096a2f95eda5e
Illia Losiei <t0955037700@gmail.com>
2024-03-23 09:19:07 +0200

    New feature2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int first = 11;
+        int second = 2;
+
+        System.out.println("Result = " + first + second);
     }
 }
